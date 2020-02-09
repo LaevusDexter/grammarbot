b8ca9472d2c08fb09c92ae653e67a97d973486a9
LaevusDexter <dkfl10011@gmail.com>
2020-02-09 22:14:53 +0500

    CorrectMatches improvements.

--- matches.go
+++ matches.go
@@ -2,9 +2,13 @@ package grammarbot
 
 // CorrectMatchesBytes - returns corrected text with first replacements in matches
 func CorrectMatchesBytes(text []byte, matches []*Match) []byte {
+
+	if len(matches) < 1 {
+		return nil
+	}
+
 	var (
 		i int
-		offset int
 		extraSize int
 	)
 
@@ -14,18 +18,15 @@ func CorrectMatchesBytes(text []byte, matches []*Match) []byte {
 
 	buf := make([]byte, len(text) + extraSize)
 
-	offset = 0
 	currentPos := 0
-	nextPos := 0
-	for i = 0; i < len(matches); i++ {
-		switch i + 1 {
-		case 1:
-			currentPos = 0
-			nextPos = matches[i].Offset
-		default:
-			currentPos = matches[i - 1].Offset +  matches[i - 1].Length
-			nextPos = matches[i].Offset
-		}
+	nextPos := matches[0].Offset
+
+	offset := copy(buf[:], text[currentPos:nextPos])
+	offset += copy(buf[offset:], matches[0].Replacements[0].Value)
+
+	for i = 1; i < len(matches); i++ {
+		currentPos = matches[i - 1].Offset +  matches[i - 1].Length
+		nextPos = matches[i].Offset
 
 		offset += copy(buf[offset:], text[currentPos:nextPos])
 		offset += copy(buf[offset:], matches[i].Replacements[0].Value)
@@ -44,9 +45,12 @@ func CorrectMatches(text string, matches []*Match) string {
 		s2b cast creates undefined behavior (runtime might ask for cap), so here's copy/paste from FixMatchesBytes
 	*/
 
+	if len(matches) < 1 {
+		return ""
+	}
+
 	var (
 		i int
-		offset int
 		extraSize int
 	)
 
@@ -56,18 +60,15 @@ func CorrectMatches(text string, matches []*Match) string {
 
 	buf := make([]byte, len(text) + extraSize)
 
-	offset = 0
 	currentPos := 0
-	nextPos := 0
-	for i = 0; i < len(matches); i++ {
-		switch i + 1 {
-		case 1:
-			currentPos = 0
-			nextPos = matches[i].Offset
-		default:
-			currentPos = matches[i - 1].Offset +  matches[i - 1].Length
-			nextPos = matches[i].Offset
-		}
+	nextPos := matches[0].Offset
+
+	offset := copy(buf[:], text[currentPos:nextPos])
+	offset += copy(buf[offset:], matches[0].Replacements[0].Value)
+
+	for i = 1; i < len(matches); i++ {
+		currentPos = matches[i - 1].Offset +  matches[i - 1].Length
+		nextPos = matches[i].Offset
 
 		offset += copy(buf[offset:], text[currentPos:nextPos])
 		offset += copy(buf[offset:], matches[i].Replacements[0].Value)
